710f6af0db1ee3d823e7350db1d344421df537a6
teodora-mustata <teodora.mustata11@gmail.com>
2025-11-28 12:39:49 +0200

    Updated API

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -5,6 +5,18 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
 // Replace appsettings.json values with Key Vault values
 var keyVaultName = builder.Configuration
     [$"AppConfigurations{ConfigurationPath.KeyDelimiter}KeyVaultName"];
@@ -48,6 +60,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("CORS");
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
